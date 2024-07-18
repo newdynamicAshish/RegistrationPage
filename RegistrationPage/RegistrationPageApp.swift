2d5e4cc278fa5647931401015b6aaf9f871ebcb2
newdynamicAshish <ashish.m@newdynamicstech.com>
2024-07-18 10:43:59 +0530

    Initial Commit

---
+++ RegistrationPage/RegistrationPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationPageApp.swift
+//  RegistrationPage
+//
+//  Created by apple on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
